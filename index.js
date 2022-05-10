2e9b3ea8e377c6290cb9a9547add4a76f128b180
jks0586 <jks0586@gmail.com>
2022-05-10 12:10:49 +0530

    update

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+type Query {
+    posts: [Post]
+    post(id: ID): Post
+    authors: [Person]
+    author(id: ID): Person
+  }
+
+  type Post {
+    id: ID
+    author: Person
+    body: String
+  }
+
+  type Person {
+    id: ID
+    posts: [Post]
+    firstName: String
+    lastName: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world! Jitendra this is first example to make this as the graphqlk exp';
+  },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
